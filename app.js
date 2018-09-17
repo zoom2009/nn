998b59254f41216fa4684adbf533c9a138579fc1
unknown <afumagic3@gmail.com>
2018-09-17 21:59:02 +0700

    1st db and api get&post

--- app.js
+++ app.js
@@ -1,48 +1,63 @@
 const express = require('express')
-const mongodb = require('mongodb')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://localhost:27017/DB').then((doc) => {
+    console.log('success to connect db')
+}, (err) => {
+    console.log('fail to connect db')
+})
+
+var Schema = mongoose.Schema
+
+var StudentSchema = new Schema({
+    id: {
+        type: String,
+        unique: true,
+        required: true,
+        minlength: 8,
+        maxlength: 8
+    },
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    age: Number
+})
+
+
+
+var Student = mongoose.model('Student', StudentSchema)
 
 var app = express() // เรียกใช้เสมอ ถ้าใช้ express
+app.use(bodyParser.json())
 
-let student = [
-    {
-        id: 'st1',name: 'aaa'
-    }, 
-    {
-        id: 'st2',name: 'bbb'    
-    }, 
-    {
-        id: 'st3',name: 'ccc'   
-    }
-]
-
-app.get('/', (req, res) => {
-    res.send('hello')
+app.get('/get', (req, res) => {
+    Student.find().then((doc) => {
+        res.send(doc)
+    }, (err) => {
+        res.status(400).send(err)
+    })
 })
 
-app.get('/all-student', (req, res) => {
-    res.send(student)
-})
-
-app.get('/all-student-id/:id', (req, res) => {
-    let studentID = req.params.id
-    for(let i=0;i<student.length;i++) {
-        if(studentID == student[i].id) {
-            res.send(student[i])
-            break
-        }
-    }
-    res.send('not found this id : '+ studentID)
-})
-
-app.get('/all-student-name/:name', (req, res) => {
-    let studentName = req.params.name
-    for(let i=0;i<student.length;i++) {
-        if(studentName == student[i].name) {
-            res.send(student[i])
-            break
-        }
-    }
-    res.send('not found this name : '+ studentName)
+app.post('/post', (req, res) => {
+    let newStudent = new Student({
+        id: req.body.id,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        age: req.body.age
+    })
+    newStudent.save().then((doc) => {
+        res.send(doc)
+    }, (err) => {
+        res.status(400).send(err)
+    })
 })
 
 
